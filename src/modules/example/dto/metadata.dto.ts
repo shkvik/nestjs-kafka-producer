101a4bdead793735c2e73e3076db36f6cffdac42
pendulum <rs.pendulum@gmail.com>
2024-10-24 14:28:44 +0300

    removed useless import

--- src/modules/example/dto/metadata.dto.ts
+++ src/modules/example/dto/metadata.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
-import { IsArray, IsString } from 'class-validator';
+import { IsString } from 'class-validator';
 
 export class MetadataDto {
   
