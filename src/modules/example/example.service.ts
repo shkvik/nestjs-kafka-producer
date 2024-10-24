5ce2b94b48925002ec28efc7e1b41a007304f750
pendulum <rs.pendulum@gmail.com>
2024-10-24 13:46:28 +0300

    fixed input & output types for emit kafka method

--- src/modules/example/example.service.ts
+++ src/modules/example/example.service.ts
@@ -3,12 +3,14 @@ import { ClientKafka } from '@nestjs/microservices';
 import { lastValueFrom } from 'rxjs';
 import { KafkaService } from '../kafka/kafka.service';
 import { CONFIG_KAFKA } from 'src/config/config.export';
+import { Message, RecordMetadata } from 'kafkajs';
 import { 
   BroadcastDto, 
   LimitDto, 
   MetadataDto, 
   QueueDto
 } from './dto';
+
 
 @Injectable()
 export class ExampleService {
@@ -28,8 +30,8 @@ export class ExampleService {
 
   public async postBroadcast(dto: BroadcastDto): Promise<string> {
     const test = await lastValueFrom(
-      this.exampleClient.emit('broadcast.topic', { 
-        message: dto.message
+      this.exampleClient.emit<RecordMetadata, Message>('broadcast.topic', { 
+        value: dto.message
       })
     );
     return JSON.stringify(test, null, 2);
@@ -37,9 +39,9 @@ export class ExampleService {
 
   public async postQueue(dto: QueueDto): Promise<string> {
     const test = await lastValueFrom(
-      this.exampleClient.emit('queue.topic', {
+      this.exampleClient.emit<RecordMetadata, Message>('queue.topic', {
         key: dto.key,
-        message: dto.message
+        value: dto.message
       })
     );
     return JSON.stringify(test, null, 2);
@@ -47,8 +49,8 @@ export class ExampleService {
 
   public async postLimit(dto: LimitDto): Promise<string>{
     const test = await lastValueFrom(
-      this.exampleClient.emit('limit.topic', {
-        message: dto.message
+      this.exampleClient.emit<RecordMetadata, Message>('limit.topic', {
+        value: dto.message,
       })
     );
     return JSON.stringify(test, null, 2);
